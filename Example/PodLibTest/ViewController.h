806f94a5bad04f98432742b328dcdd16981e406a
ke he <smileshitou@hotmail.com>
2017-07-25 22:50:17 +0800

    for test

---
+++ Example/PodLibTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodLibTest
+//
+//  Created by heke on 2017/7/25.
+//  Copyright © 2017年 MX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/Pods/Target Support Files/Pods-PodLibTest/Pods-PodLibTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodLibTest : NSObject
+@end
+@implementation PodsDummy_Pods_PodLibTest
+@end
